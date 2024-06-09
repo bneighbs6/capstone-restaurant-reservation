7a8ef28495a1a337443f68beb823f612dd947749
bneighbs6 <152036405+bneighbs6@users.noreply.github.com>
2024-06-09 10:22:40 -0600

    Update TableCreate

--- front-end/src/tables/TableCreate.jsx
+++ front-end/src/tables/TableCreate.jsx
@@ -1,19 +1,37 @@
 function TableCreate() {
-    return (
-        <>
-            <h1>Reserved Tables</h1>
-            <input 
-            name="table_name"
-            placeholder="Table Name" 
-            />
-            <input 
-            name="capacity" 
-            placeholder="Table Capacity"
-            />
-            <button className="btn btn-success">Submit</button>
-            <button className="btn btn-danger">Cancel</button>
-        </>
-    )
+
+  function submitHandler() {
+    console.log("Submit handled.");
+  }
+
+  return (
+    <>
+      <h1>Reserved Tables</h1>
+      <form onSubmit={submitHandler} className="">
+
+        <div className="form-group">
+          <label htmlFor="table_name">Table Name</label>
+          <input 
+          name="table_name" 
+          placeholder="Table Name" 
+          className="form-control"
+          />
+          <label htmlFor="capacity">Table Capacity</label>
+          <input 
+          name="capacity" 
+          placeholder="Table Capacity" 
+          className="form-control"
+          />
+        </div>
+
+        <div className="form-group">
+          <button className="btn btn-success">Submit</button>
+          <button className="btn btn-danger">Cancel</button>
+        </div>
+
+      </form>
+    </>
+  );
 }
 
-export default TableCreate; 
+export default TableCreate;
