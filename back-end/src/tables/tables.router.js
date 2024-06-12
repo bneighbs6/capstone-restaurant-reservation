a66262f1516e21b552b3051ed99170a994a1aaec
bneighbs6 <152036405+bneighbs6@users.noreply.github.com>
2024-06-12 15:58:44 -0600

    created readTable & readReservation in tables.service

--- back-end/src/tables/tables.router.js
+++ back-end/src/tables/tables.router.js
@@ -2,6 +2,11 @@ const router = require("express").Router();
 const controller = require("./tables.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
 
+// TODO: This route will need a put method and a delete method
+// Working on test: Seat reservation PUT /tables/:table_id/seat
+router.route("/:table_id/seat").put(controller.update).all(methodNotAllowed);
+
+// This route is completed for backend tests
 router.route("/").get(controller.list).post(controller.create).all(methodNotAllowed);
 
 module.exports = router; 
--- back-end/src/tables/tables.service.js
+++ back-end/src/tables/tables.service.js
@@ -8,11 +8,24 @@ function create(newTable) {
     .then((createdTable) => createdTable[0]);
 }
 
+// Reads table by table id
+function readTable(table_id) {
+    return knex("tables").select("*").where({ table_id }).first(); 
+}
+
+// Reads reservation by reservation id
+function readReservation(reservation_id) {
+    return knex("reservations").select("*").where({ reservation_id }).first(); 
+} 
+
+// Lists tables by table name
 function list() {
     return knex("tables").select("*").orderBy("table_name");
 }
 
 module.exports = {
     create,
+    readTable,
+    readReservation, 
     list,
 }
