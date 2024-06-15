5c30d5036fc09fcc5f442aad002d487e67260567
bneighbs6 <152036405+bneighbs6@users.noreply.github.com>
2024-06-15 12:23:25 -0600

    Created DashboardTablesTable

---
+++ front-end/src/dashboard/DashboardTablesTable.jsx
@@ -0,0 +1,77 @@
+import React, { useEffect, useState } from "react";
+import { listTables, deleteTableAssignment } from "../utils/api";
+import ErrorAlert from "../layout/ErrorAlert";
+
+function DashboardTablesTable({ loadReservations }) {
+  const [tables, setTables] = useState([]);
+  const [tableErrors, setTableErrors] = useState(null);
+
+  useEffect(loadTables, []);
+
+  function loadTables() {
+    const abortController = new AbortController();
+    setTableErrors(null);
+    listTables(abortController.signal).then(setTables).catch(setTableErrors);
+    return () => abortController.abort();
+  }
+
+  const tablesListItem = tables.map((table) => {
+    let tableStatus = "Free";
+    if (table.reservation_id) {
+      tableStatus = "Occupied";
+    }
+
+    let occupant = null;
+    if (tableStatus === "Occupied") {
+      occupant = ` by Reservation ID: ${table.reservation_id}`;
+    }
+
+    return (
+      <li className="list-group-item col" key={table.table_id}>
+        <div>{table.table_name}</div>
+        <p data-table-id-status={`${table.table_id}`}>
+          {tableStatus}
+          {occupant}
+        </p>
+        <FinishButton tableStatus={tableStatus} tableId={table.table_id} />
+      </li>
+    );
+  });
+
+  function FinishButton({ tableStatus, tableId }) {
+    if (tableStatus === "Occupied") {
+      return (
+        <button
+        type="button"
+        className="btn btn-success"
+        data-table-id-finish={tableId}
+        onClick={handleFinishButtonClick(tableId)}
+        >
+          Finish
+        </button>
+      );
+    }
+    return null;
+  }
+
+  async function handleFinishButtonClick(tableId) {
+    if (window.confirm("Is this table ready to seat new guests? This cannot be undone.")) {
+      await deleteTableAssignment(tableId);
+      loadTables();
+      loadReservations();
+    }
+  }
+
+  return (
+    <>
+      <ErrorAlert error={tableErrors} />
+      <div>
+        <h4>Tables</h4>
+        <ul>{tablesListItem}</ul>
+      </div>
+    </>
+  )
+
+}
+
+export default DashboardTablesTable;
