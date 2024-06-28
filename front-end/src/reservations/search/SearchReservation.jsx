4274c860ce024d45cbc499d444766ffc91bf0b39
bneighbs6 <152036405+bneighbs6@users.noreply.github.com>
2024-06-28 16:47:58 -0600

    Updated SearchReservation.jsx

--- front-end/src/reservations/search/SearchReservation.jsx
+++ front-end/src/reservations/search/SearchReservation.jsx
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import ErrorAlert from "../../layout/ErrorAlert";
+import { listReservations } from "../../utils/api";
+import DashboardReservationsTable from "../../dashboard/DashboardReservationsTable";
 
-function SearchReservation({ reservation }) {
+function SearchReservation() {
+
+  // These will need to be changed using useState
+  const [phoneNumber, setPhoneNumber] = useState({
+    mobile_number: "",
+  });
+  const [foundReservation, setFoundReservation] = useState(null);
+  const [error, setError] = useState(null);
 
     function findButtonHandler() {
         console.log("Find button clicked.")
         // Enter necessary code
+        // Clicking on the "Find" button will submit a request to the server 
+        // (e.g. GET /reservations?mobile_number=800-555-1212).
+    }
+
+    function changeHandler({ target: { name, value } }) {
+      setPhoneN
     }
 
 
