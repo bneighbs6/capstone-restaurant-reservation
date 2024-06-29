78506eefd5ec52080f3311af7310a971236dad12
bneighbs6 <152036405+bneighbs6@users.noreply.github.com>
2024-06-28 21:04:29 -0600

    Added search to reservations.service

--- back-end/src/reservations/reservations.service.js
+++ back-end/src/reservations/reservations.service.js
@@ -2,20 +2,21 @@ const knex = require("../db/connection");
 
 // Creates a new reservation
 function create(newReservation) {
-    return knex("reservations")
+  return knex("reservations")
     .insert(newReservation)
     .returning("*")
     .then((createdRecord) => createdRecord[0]);
 }
 
+// Reads reservation by reservation_id
 function read(reservation_id) {
-    return knex("reservations as r") 
+  return knex("reservations as r")
     .select("*")
     .where({ reservation_id })
     .first();
 }
 
-
+// Updates a reservation
 function update(updatedReservation) {
   return knex("reservations")
     .select("*")
@@ -26,16 +27,26 @@ function update(updatedReservation) {
 
 // Lists reservations by date and orders by time
 function listReservationsByDate(reservation_date) {
-    return knex("reservations")
-      .select("*")
-      .where({ reservation_date: reservation_date })
-      .whereNot({ status: "finished" })
-      .orderBy("reservation_time");
-  }
+  return knex("reservations")
+    .select("*")
+    .where({ reservation_date: reservation_date })
+    .whereNot({ status: "finished" })
+    .orderBy("reservation_time");
+}
+
+function search(mobile_number) {
+  return knex("reservations")
+    .whereRaw(
+      "translate(mobile_number, '() -', '') like ?",
+      `%${mobile_number.replace(/\D/g, "")}%`
+    )
+    .orderBy("reservation_date");
+}
 
 module.exports = {
-    create,
-    read,
-    update,
-    listReservationsByDate,
-}
+  create,
+  read,
+  update,
+  listReservationsByDate,
+  search,
+};
