34bd7d47e741ce70703c79e1ca14166f5f0aabc3
bneighbs6 <152036405+bneighbs6@users.noreply.github.com>
2024-07-02 15:37:09 -0600

    Put buttons into own <td>

--- front-end/src/dashboard/DashboardReservationsTable.jsx
+++ front-end/src/dashboard/DashboardReservationsTable.jsx
@@ -4,148 +4,144 @@ import ErrorAlert from "../layout/ErrorAlert";
 import { setReservationStatus } from "../utils/api";
 import { formatAsTime } from "../utils/date-time";
 
-
 function DashboardReservationsTable({ reservations, loadDashboard }) {
-  const location = useLocation(); 
+  const location = useLocation();
 
   const [error, setError] = useState(null);
 
-    const rows =
-      Array.isArray(reservations) &&
-      reservations.map((reservation, index) => {
+  const rows =
+    Array.isArray(reservations) &&
+    reservations.map((reservation, index) => {
+      if (
+        location.pathname === "/dashboard" &&
+        (reservation.status === "finished" ||
+          reservation.status === "cancelled")
+      ) {
+        return null;
+      }
 
-        if (
-          location.pathname === "/dashboard" &&
-          (reservation.status === "finished" ||
-            reservation.status === "cancelled")
-        ) {
-          return null;
-        }
+      return (
+        <>
+          <tr key={reservation.reservation_id}>
+            <th scope="row">{formatAsTime(reservation.reservation_time)}</th>
+            <td>{reservation.reservation_id}</td>
+            <td>{reservation.first_name}</td>
+            <td>{reservation.last_name}</td>
+            <td>{reservation.mobile_number}</td>
+            <td>{reservation.people}</td>
+            <td data-reservation-id-status={reservation.reservation_id}>
+              {reservation.status}
+            </td>
+            <td>
+              <EditReservationButton
+                reservation_id={reservation.reservation_id}
+              />
 
-        return (
-          <>
-                <tr key={reservation.reservation_id}>
-                  <th scope="row">{formatAsTime(reservation.reservation_time)}</th>
-                  <td>{reservation.reservation_id}</td>
-                  <td>{reservation.first_name}</td>
-                  <td>{reservation.last_name}</td>
-                  <td>{reservation.mobile_number}</td>
-                  <td>{reservation.people}</td>
-                  <td data-reservation-id-status={reservation.reservation_id}>
-                    {reservation.status}
-                  </td>
-                </tr>
-            <div>
-            <EditReservationButton
-                    reservation_id={reservation.reservation_id}
-                  />
-            </div>
-            <div>
-            <CancelReservationButton reservation={reservation} />
-            </div>
-            <div>
+              <CancelReservationButton reservation={reservation} />
+
               <SeatReservationButton
                 reservation_id={reservation.reservation_id}
                 status={reservation.status}
               />
-            </div>
-          </>
-        );
+            </td>
+          </tr>
+        </>
+      );
 
-        function SeatReservationButton({ reservation_id, status }) {
-          if (status === "booked") {
-            return (
-              <a
-                className="btn btn-primary"
-                href={`/reservations/${reservation_id}/seat`}
-                role="button"
-              >
-                Seat Reservation
-              </a>
-            );
-          } else {
-            return null;
-          }
-        }
-
-        // onClick () => take you to /reservations/${reservation_id}/edit page
-        function EditReservationButton({ reservation_id }) {
+      function SeatReservationButton({ reservation_id, status }) {
+        if (status === "booked") {
           return (
             <a
-              className="btn btn-warning"
-              href={`/reservations/${reservation_id}/edit`}
+              className="btn btn-primary btn-sm col-md-8"
+              href={`/reservations/${reservation_id}/seat`}
               role="button"
             >
-              Edit Reservation
+              Seat Reservation
             </a>
           );
+        } else {
+          return null;
         }
+      }
 
-        function CancelReservationButton({ reservation }) {
-          return (
-            <button
-              type="button"
-              className="btn btn-danger"
-              data-reservation-id-cancel={reservation.reservation_id}
-              onClick={() =>
-                handleCancelReservationButtonClick(reservation.reservation_id)
-              }
-            >
-              Cancel Reservation
-            </button>
-          );
+      // onClick () => take you to /reservations/${reservation_id}/edit page
+      function EditReservationButton({ reservation_id }) {
+        return (
+          <a
+            className="btn btn-secondary btn-sm col-md-8"
+            href={`/reservations/${reservation_id}/edit`}
+            role="button"
+          >
+            Edit Reservation
+          </a>
+        );
+      }
+
+      function CancelReservationButton({ reservation }) {
+        return (
+          <button
+            type="button"
+            className="btn btn-danger btn-sm col-md-8"
+            data-reservation-id-cancel={reservation.reservation_id}
+            onClick={() =>
+              handleCancelReservationButtonClick(reservation.reservation_id)
+            }
+          >
+            Cancel Reservation
+          </button>
+        );
+      }
+
+      function handleCancelReservationButtonClick(reservation_id) {
+        if (
+          window.confirm(
+            "Do you want to cancel this reservation? This cannot be undone."
+          )
+        ) {
+          const abortController = new AbortController();
+          setError(null);
+          setReservationStatus(
+            reservation_id,
+            "cancelled",
+            abortController.signal
+          )
+            .then(() => loadDashboard())
+            .catch(setError);
+          return () => abortController.abort();
         }
+      }
+    });
 
-        function handleCancelReservationButtonClick(reservation_id) {
-          if (
-            window.confirm(
-              "Do you want to cancel this reservation? This cannot be undone."
-            )
-          ) {
-            const abortController = new AbortController();
-            setError(null);
-            setReservationStatus(
-              reservation_id,
-              "cancelled",
-              abortController.signal
-            )
-              .then(() => loadDashboard())
-              .catch(setError);
-            return () => abortController.abort();
-          }
-        }
-      });
-
-    if (reservations && !reservations.length) {
-      return (
-        <div className="alert alert-warning py-3" role="alert">
-          No reservations found.
-        </div>
-      );
-    }
-  
+  if (reservations && !reservations.length) {
     return (
-      <>
-        <ErrorAlert error={error} />
-        <div class="table-responsive">
-          <table className="table table-secondary table-hover align-middle">
-            <thead className="table-primary">
-              <tr>
-                <th>Reservation Time</th>
-                <th>Reservation ID</th>
-                <th>First Name</th>
-                <th>Last Name</th>
-                <th>Mobile Number</th>
-                <th>Number of People</th>
-                <th>Reservation Time</th>
-                <th>Reservation Status</th>
-              </tr>
-            </thead>
-            <tbody>{rows}</tbody>
-          </table>
-        </div>
-      </>
+      <div className="alert alert-warning py-3" role="alert">
+        No reservations found.
+      </div>
     );
   }
+
+  return (
+    <>
+      <ErrorAlert error={error} />
+      <div class="table-responsive">
+        <table className="table table-secondary table-hover align-middle">
+          <thead className="table-primary">
+            <tr>
+              <th>Reservation Time</th>
+              <th>Reservation ID</th>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Mobile Number</th>
+              <th>Number of People</th>
+              <th>Reservation Status</th>
+              <th>Reservation Control</th>
+            </tr>
+          </thead>
+          <tbody>{rows}</tbody>
+        </table>
+      </div>
+    </>
+  );
+}
 
 export default DashboardReservationsTable;
